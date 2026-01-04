1d46ff8d903d637e769c8af629b56d6bd5ed02a9
DESKTOP-BMQPELL\claudio <claudiolaguzzi@gmail.com>
2026-01-04 02:12:40 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app.jsx' 
+import App from './app'
 import { BrowserRouter } from 'react-router-dom'
 
 
